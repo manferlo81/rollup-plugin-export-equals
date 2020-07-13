0ddb6daf3cc59cb76d45c779a54dc191deea8030
Manuel Fernandez <manferlo81@gmail.com>
2020-07-14 02:18:43 +0700

    chore: Use @typescript-eslint 3

--- __test__/tools/generate.ts
+++ __test__/tools/generate.ts
@@ -2,7 +2,7 @@ import type { Plugin } from 'rollup';
 import equals from '../../src';
 import rollup from './rollup';
 
-async function generate(filename: string, plugins: Plugin[], options?: equals.ExportEqualsOptions) {
+async function generate(filename: string, plugins: Plugin[], options?: equals.ExportEqualsOptions): Promise<string> {
   const build = await rollup(filename, plugins, options);
   const { output: [{ code }] } = await build.generate({ file: 'output.d.ts' });
   return code;
--- __test__/tools/rollup.ts
+++ __test__/tools/rollup.ts
@@ -3,7 +3,7 @@ import { rollup as rollupBuild } from 'rollup';
 import type { Plugin } from 'rollup';
 import equals from '../../src';
 
-function rollup(filename: string, plugins: Plugin[], options?: equals.ExportEqualsOptions) {
+function rollup(filename: string, plugins: Plugin[], options?: equals.ExportEqualsOptions): ReturnType<typeof rollupBuild> {
   return rollupBuild({
     input: path.resolve(__dirname, '../examples', filename),
     plugins: [
