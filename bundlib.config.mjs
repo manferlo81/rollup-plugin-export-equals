58a43a4a2064171865a7c95a328fe5d74e0a81d6
Manuel Fernández <manferlo81@gmail.com>
2024-08-21 21:35:44 +0700

    chore: Update and setup babel

---
+++ bundlib.config.mjs
@@ -0,0 +1,9 @@
+/** @type { import("bundlib").BundlibConfig } */
+const config = {
+  esModule: true,
+  interop: true,
+  project: "tsconfig-build.json",
+  equals: true,
+};
+
+export default config;
