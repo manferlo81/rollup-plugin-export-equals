55ec2e5bb852166a3828169318dfbc2b366e820b
Manuel Fernández <manferlo81@gmail.com>
2024-10-09 13:13:34 +0700

    feat: Matches extra spaces and optional ones

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,6 @@
-import { readFile, writeFile } from 'fs/promises';
 import type { Plugin } from 'rollup';
-import type { ExportEqualsOptions, ReplaceFunction } from './types';
+import { createCodeTransformer, replaceFileContent } from './replace';
+import type { ExportEqualsOptions } from './types';
 
 export function equals(options: ExportEqualsOptions = {}): Plugin {
 
@@ -9,46 +9,15 @@ export function equals(options: ExportEqualsOptions = {}): Plugin {
     replace = 'export = $1',
   } = options;
 
-  const captureIdPattern = '([a-zA-Z$_][\\w$_]*)';
-
-  const strings = [
-    `export default ${captureIdPattern}`,
-    `export { ${captureIdPattern} as default }`,
-  ];
-
-  const patterns = strings.map((str) => new RegExp(str));
-
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  const replaceExport = (code: string): string | void => {
-    for (const pattern of patterns) {
-      if (pattern.test(code)) {
-        return code.replace(pattern, replace as string & ReplaceFunction);
-      }
-    }
-  };
-
   const name = 'export-equals';
+  const transformCode = createCodeTransformer(replace);
 
   if (!filename) {
-    return { name, renderChunk: replaceExport };
+    return { name, renderChunk: transformCode };
   }
 
   const writeBundle = async () => {
-
-    const content = await readFile(
-      filename,
-      'utf-8',
-    );
-
-    const modifiedCode = replaceExport(content);
-
-    if (!modifiedCode) return;
-
-    await writeFile(
-      filename,
-      modifiedCode,
-    );
-
+    await replaceFileContent(filename, transformCode);
   };
 
   return { name, writeBundle };
---
+++ src/replace.ts
@@ -0,0 +1,50 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import type { ReplaceFunction } from './types';
+
+/** Matches a space */
+const space = '\\s';
+
+/** Matches any number of spaces */
+const optionalSpace = `${space}*`;
+
+/** Matches one or more spaces */
+const requiredSpace = `${space}+`;
+
+/** Matches and captures a valid javascript id */
+const captureIdPattern = '([a-zA-Z$_][\\w$_]*)';
+
+const patternStrings = [
+  `export${requiredSpace}default${requiredSpace}${captureIdPattern}`,
+  `export${optionalSpace}{${optionalSpace}${captureIdPattern}${requiredSpace}as${requiredSpace}default${optionalSpace}}`,
+];
+
+const createRegExpFromString = (str: string): RegExp => new RegExp(str);
+const patterns = patternStrings.map(createRegExpFromString);
+
+type TransformCodeFunction = (code: string) => string | undefined;
+
+export function createCodeTransformer(replacer: ReplaceFunction | string): TransformCodeFunction {
+  return (code) => {
+    for (const pattern of patterns) {
+      if (pattern.test(code)) return code.replace(pattern, replacer as never);
+    }
+  };
+}
+
+export async function replaceFileContent(filename: string, transform: TransformCodeFunction) {
+
+  const content = await readFile(
+    filename,
+    'utf-8',
+  );
+
+  const modifiedCode = transform(content);
+
+  if (!modifiedCode) return;
+
+  await writeFile(
+    filename,
+    modifiedCode,
+  );
+
+}
