b8a1cb26134d58ff4c0de65b53b68250afc21093
Manuel Fernández <manferlo81@gmail.com>
2025-05-05 09:41:39 +0700

    chore: Rename tsconfig files

--- bundlib.config.mjs
+++ bundlib.config.mjs
@@ -3,6 +3,6 @@ import { config } from 'bundlib'
 export default config({
   esModule: true,
   interop: true,
-  project: 'tsconfig-build.json',
+  project: './tsconfig.build.json',
   equals: true,
 })
